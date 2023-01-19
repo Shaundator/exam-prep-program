2d1c9335baecb9d079c0fb0b0dffdc47a4eda193
Shaundator <90447626+Shaundator@users.noreply.github.com>
2023-01-19 20:15:13 +0100

    commit 4:

--- src/main/java/com/example/examprepprogram/greeting/GreetingController.java
+++ src/main/java/com/example/examprepprogram/greeting/GreetingController.java
@@ -1,18 +1,22 @@
 package com.example.examprepprogram.greeting;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class GreetingController {
 
     private GreetingRepository greetingRepository;
+
     @GetMapping("/hello")
     private ResponseEntity<Greeting> hello(){
         Greeting greeting = new Greeting("Hello There");
+        greetingRepository.save(greeting);
         return new ResponseEntity<>(greeting, HttpStatus.OK);
     }
 
